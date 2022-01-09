bf74085245ea10649feba183a1c5636cf24dff47
nagababu@usf.edu <nagababu@usf.edu>
2022-01-08 22:08:06 -0500

    * ISM6225_Assignment_2_Spring_2022.sln: * Program.cs: * ISM6225_Assignment_2_Spring_2022.csproj:
    * .gitignore: Final changes

---
+++ ISM6225_Assignment_2_Spring_2022/Program.cs
@@ -0,0 +1,429 @@
+﻿/* 
+ 
+YOU ARE NOT ALLOWED TO MODIFY ANY FUNCTION DEFINATION's PROVIDED.
+WRITE YOUR CODE IN THE RESPECTIVE QUESTION FUNCTION BLOCK
+
+*/
+
+using System;
+using System.Collections.Generic;
+
+namespace ISM6225_Assignment_2_Spring_2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Question1:
+
+            Console.WriteLine("Question 1");
+            int[] nums = {1,2,2,1};
+            int k = 1;
+            int number_pairs = CountKDifference(nums, k);
+            Console.WriteLine("The number of pairs with absolute difference as k are {0}", number_pairs);
+            Console.WriteLine();
+
+            //Question 2:
+            Console.WriteLine("Question 2:");
+            int[] nums1 = { 0, 1, 0, 3, 12 };
+            Console.WriteLine("Enter the target number:");
+            int target = Int32.Parse(Console.ReadLine());
+            int pos = SearchInsert(nums1, target);
+            Console.WriteLine("Insert Position of the target is : {0}", pos);
+            Console.WriteLine("");
+
+            //Question3:
+            Console.WriteLine("Question 3");
+            string paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.";
+            string[] banned = { "hit" };
+            string commonWord = MostCommonWord(paragraph, banned);
+            Console.WriteLine("Most frequent word is {0}:", commonWord);
+            Console.WriteLine();
+
+            //Question 4:
+            Console.WriteLine("Question 4");
+            int[] arr1 = {2,2,3,4};
+            int lucky_number = FindLucky(arr1);
+            Console.WriteLine("The Lucky number in the given array is {0}",lucky_number);
+            Console.WriteLine();
+
+            //Question 5:
+            Console.WriteLine("Question 5");
+            string secret = "1807";
+            string guess = "7810";
+            string hint = GetHint(secret,guess);
+            Console.WriteLine("Hint for the guess is :{0}", hint);
+
+            Console.WriteLine();
+
+            //Question 6:
+            Console.WriteLine("Question 6");
+            int[] Nums = { 2, 7, 11, 15 };
+            int target_sum = 9;
+            int[] indexes=targetSum(Nums, target_sum);
+            Console.WriteLine("Index locations are: {0} {1}", indexes[0], indexes[1]);
+            Console.WriteLine();
+
+            //Question 7:
+
+            Console.WriteLine("Question 7:");
+            string text = "hello world";
+            string brokenLetters = "ad";
+            int count = CanBeTypedWords(text,brokenLetters);
+            Console.WriteLine("Number of words you can fully type are : {0}", count);
+            Console.WriteLine(" ");
+
+            //Question 8:
+            Console.WriteLine("Question 8");
+            string s = "ababcbacadefegdehijhklij";
+            List<int> part = PartitionLabels( s);
+            Console.WriteLine("partation lengths are:");
+            for (int i = 0; i < part.Count; i++)
+            {
+                Console.Write(part[i] + "\t");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //Question 9:
+            Console.WriteLine("Question 9");
+            int[,] grid = { { 0, 1, 2, 3, 4 }, { 24, 23, 22, 21, 5 }, { 12, 13, 14, 15, 16 }, { 11, 17, 18, 19, 20 }, { 10, 9, 8, 7, 6 } };
+            int time = SwimInWater(grid);
+            Console.WriteLine("Minimum time required is: {0}", time);
+            Console.WriteLine();
+
+            //Question 10:
+            Console.WriteLine("Question 10");
+            string word1  = "horse";
+            string word2 = "ros";
+            int minLen = MinDistance( word1,  word2);
+            Console.WriteLine("Minimum number of operations required are {0}", minLen);
+            Console.WriteLine();
+        }
+
+
+        /*
+        Given an integer array nums and an integer k, return the number of pairs (i, j) where i < j such that |nums[i] - nums[j]| == k.
+        The value of |x| is defined as:
+            •	x if x >= 0.
+            •	-x if x < 0.
+        Example 1:
+        Input: nums = [1,2,2,1], k = 1
+        Output: 4
+        Explanation: The pairs with an absolute difference of 1 are:
+        - [1,2,2,1]
+        - [1,2,2,1]
+        - [1,2,2,1]
+        */
+
+        public static int CountKDifference(int[] nums, int k)
+        {
+            try
+            {
+                //write your code here.
+                return 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        /*
+        
+        Question 2:
+        Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+        Note: The algorithm should have run time complexity of O (log n).
+        Hint: Use binary search
+        Example 1:
+        Input: nums = [1,3,5,6], target = 5
+        Output: 2
+        Example 2:
+        Input: nums = [1,3,5,6], target = 2
+        Output: 1
+        Example 3:
+        Input: nums = [1,3,5,6], target = 7
+        Output: 4
+        */
+
+        public static int SearchInsert(int[] nums, int target)
+        {
+            try
+            {
+                //Write your Code here.
+                return -1;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        /*
+         
+        Question 3
+       
+        Given a string paragraph and a string array of the banned words banned, return the most frequent word that is not banned. It is guaranteed there is at least one word that is not banned, and that the answer is unique.
+        The words in paragraph are case-insensitive and the answer should be returned in lowercase.
+
+        Example 1:
+        Input: paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.", banned = ["hit"]
+        Output: "ball"
+        Explanation: "hit" occurs 3 times, but it is a banned word. "ball" occurs twice (and no other word does), so it is the most frequent non-banned word in the paragraph. 
+        Note that words in the paragraph are not case sensitive, that punctuation is ignored (even if adjacent to words, such as "ball,"), and that "hit" isn't the answer even though it occurs more because it is banned.
+
+        Example 2:
+        Input: paragraph = "a.", banned = []
+        Output: "a"
+        */
+
+        public static string MostCommonWord(string paragraph, string[] banned)
+        {
+            try
+            {
+                
+                //write your code here.
+
+                return "";
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        /*
+        Question 4:
+        Given an array of integers arr, a lucky integer is an integer that has a frequency in the array equal to its value.
+        Return the largest lucky integer in the array. If there is no lucky integer return -1.
+ 
+        Example 1:
+        Input: arr = [2,2,3,4]
+        Output: 2
+        Explanation: The only lucky number in the array is 2 because frequency[2] == 2.
+
+        Example 2:
+        Input: arr = [1,2,2,3,3,3]
+        Output: 3
+        Explanation: 1, 2 and 3 are all lucky numbers, return the largest of them.
+
+        Example 3:
+        Input: arr = [2,2,2,3,3]
+        Output: -1
+        Explanation: There are no lucky numbers in the array.
+         */
+
+        public static int FindLucky(int[] arr)
+        {
+            try
+            {
+                //write your code here.
+                return 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        /*
+        
+        Question 5:
+        You are playing the Bulls and Cows game with your friend.
+        You write down a secret number and ask your friend to guess what the number is. When your friend makes a guess, you provide a hint with the following info:
+        •	The number of "bulls", which are digits in the guess that are in the correct position.
+        •	The number of "cows", which are digits in the guess that are in your secret number but are located in the wrong position. Specifically, the non-bull digits in the guess that could be rearranged such that they become bulls.
+        Given the secret number secret and your friend's guess guess, return the hint for your friend's guess.
+        The hint should be formatted as "xAyB", where x is the number of bulls and y is the number of cows. Note that both secret and guess may contain duplicate digits.
+ 
+        Example 1:
+        Input: secret = "1807", guess = "7810"
+        Output: "1A3B"
+        Explanation: Bulls relate to a '|' and cows are underlined:
+        "1807"
+          |
+        "7810"
+
+        */
+
+        public static string GetHint(string secret, string guess)
+        {
+            try
+            {
+                //write your code here.
+                return "";
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        /*
+        
+        Question 6:
+        
+        Given an array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number.
+        Print the indices of the two numbers (1-indexed) as an integer array answer of size 2, where 1 <= answer[0] < answer[1] <= numbers. Length.
+        You may not use the same element twice, there will be only one solution for a given array.
+        Note: Solve it in O(n) and O(1) space complexity.
+        Hint: Use the fact that array is sorted in ascending order and there exists only one solution.
+        Typically, in these cases it’s useful to use pointers tracking the two ends of the array.
+        Example 1:
+        Input: numbers = [2,7,11,15], target = 9
+        Output: [1,2]
+        Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+        Example 2:
+        Input: numbers = [2,3,4], target = 6
+        Output: [1,3]
+        Example 3:
+        Input: numbers = [-1,0], target = -1
+        Output: [1,2]
+        */
+
+        public static int[] targetSum(int[] nums, int target)
+        {
+            try
+            {
+                //write your code here.
+                //print the answer in the function itself.
+                return new int[] {0,0};
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        /*
+         
+        Question 7:
+        There is a malfunctioning keyboard where some letter keys do not work. All other keys on the keyboard work properly.
+        Given a string text of words separated by a single space (no leading or trailing spaces) and a string brokenLetters of all distinct letter keys that are broken, return the number of words in text you can fully type using this keyboard.
+ 
+        Example 1:
+        Input: text = "hello world", brokenLetters = "ad"
+        Output: 1
+        Explanation: We cannot type "world" because the 'd' key is broken.
+
+        Example 2:
+        Input: text = "leet code", brokenLetters = "lt"
+        Output: 1
+        Explanation: We cannot type "leet" because the 'l' and 't' keys are broken.
+
+        */
+
+        public static int CanBeTypedWords(string text, string brokenLetters)
+        {
+            try
+            {
+                //write your code here.
+                return 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        /*
+        Question 8:
+        You are given a string s. We want to partition the string into as many parts as possible so that each letter appears in at most one part.
+        Return a list of integers representing the size of these parts.
+
+        Example 1:
+        Input: s = "ababcbacadefegdehijhklij"
+        Output: [9,7,8]
+        Explanation:
+        The partition is "ababcbaca", "defegde", "hijhklij".
+        This is a partition so that each letter appears in at most one part.
+        A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits s into less parts.
+
+        */
+
+        public static List<int> PartitionLabels(string s)
+        {
+            try
+            {
+                //write your code here.
+                
+                return new List<int>() { } ;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        /*
+        
+        Question 9:
+        You are given an n x n integer matrix grid where each value grid[i][j] represents the elevation at that point (i, j).
+        The rain starts to fall. At time t, the depth of the water everywhere is t. You can swim from a square to another 4-directionally adjacent square if and only if the elevation of both squares individually are at most t. You can swim infinite distances in zero time. Of course, you must stay within the boundaries of the grid during your swim.
+        Return the least time until you can reach the bottom right square (n - 1, n - 1) if you start at the top left square (0, 0).
+
+        Example 1:
+        Input: grid = [[0,1,2,3,4],[24,23,22,21,5],[12,13,14,15,16],[11,17,18,19,20],[10,9,8,7,6]]
+        Output: 16
+        Explanation: The final route is shown.
+        We need to wait until time 16 so that (0, 0) and (4, 4) are connected.
+
+        */
+
+        public static int SwimInWater(int[,] grid)
+        {
+            try
+            {
+                //write your code here.
+                return 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        /*
+         
+        Question 10:
+        Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
+        You have the following three operations permitted on a word:
+
+        •	Insert a character
+        •	Delete a character
+        •	Replace a character
+         Note: Try to come up with a solution that has polynomial runtime, then try to optimize it to quadratic runtime.
+
+        Example 1:
+        Input: word1 = "horse", word2 = "ros"
+        Output: 3
+        Explanation: 
+        horse -> rorse (replace 'h' with 'r')
+        rorse -> rose (remove 'r')
+        rose -> ros (remove 'e')
+
+        */
+
+        public static int MinDistance(string word1, string word2)
+        {
+            try
+            {
+                //write your code here.
+                return 0;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
