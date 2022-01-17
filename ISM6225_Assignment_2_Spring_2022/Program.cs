7d8925328d59781a0d60a2f4fcbde5bed8a22c70
nagababu@usf.edu <nagababu@usf.edu>
2022-01-17 00:52:02 -0500

    Added Questions

--- ISM6225_Assignment_2_Spring_2022/Program.cs
+++ ISM6225_Assignment_2_Spring_2022/Program.cs
@@ -17,7 +17,7 @@ namespace ISM6225_Assignment_2_Spring_2022
             //Question1:
 
             Console.WriteLine("Question 1");
-            int[] nums = {1,2,2,1};
+            int[] nums = { 1, 2, 2, 1 };
             int k = 1;
             int number_pairs = CountKDifference(nums, k);
             Console.WriteLine("The number of pairs with absolute difference as k are {0}", number_pairs);
@@ -42,16 +42,16 @@ namespace ISM6225_Assignment_2_Spring_2022
 
             //Question 4:
             Console.WriteLine("Question 4");
-            int[] arr1 = {2,2,3,4};
+            int[] arr1 = { 2, 2, 3, 4 };
             int lucky_number = FindLucky(arr1);
-            Console.WriteLine("The Lucky number in the given array is {0}",lucky_number);
+            Console.WriteLine("The Lucky number in the given array is {0}", lucky_number);
             Console.WriteLine();
 
             //Question 5:
             Console.WriteLine("Question 5");
             string secret = "1807";
             string guess = "7810";
-            string hint = GetHint(secret,guess);
+            string hint = GetHint(secret, guess);
             Console.WriteLine("Hint for the guess is :{0}", hint);
 
             Console.WriteLine();
@@ -60,7 +60,7 @@ namespace ISM6225_Assignment_2_Spring_2022
             Console.WriteLine("Question 6");
             int[] Nums = { 2, 7, 11, 15 };
             int target_sum = 9;
-            int[] indexes=targetSum(Nums, target_sum);
+            int[] indexes = targetSum(Nums, target_sum);
             Console.WriteLine("Index locations are: {0} {1}", indexes[0], indexes[1]);
             Console.WriteLine();
 
@@ -69,14 +69,14 @@ namespace ISM6225_Assignment_2_Spring_2022
             Console.WriteLine("Question 7:");
             string text = "hello world";
             string brokenLetters = "ad";
-            int count = CanBeTypedWords(text,brokenLetters);
+            int count = CanBeTypedWords(text, brokenLetters);
             Console.WriteLine("Number of words you can fully type are : {0}", count);
             Console.WriteLine(" ");
 
             //Question 8:
             Console.WriteLine("Question 8");
             string s = "ababcbacadefegdehijhklij";
-            List<int> part = PartitionLabels( s);
+            List<int> part = PartitionLabels(s);
             Console.WriteLine("partation lengths are:");
             for (int i = 0; i < part.Count; i++)
             {
@@ -87,13 +87,88 @@ namespace ISM6225_Assignment_2_Spring_2022
 
             //Question 9:
             Console.WriteLine("Question 9");
+            int[] widths = new int[] { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 };
+            string bulls_string9 = "abcdefghijklmnopqrstuvwxyz";
+            List<int> lines = NumberOfLines(widths, bulls_string9);
+            Console.WriteLine("Lines Required to print:");
+            for (int i = 0; i < lines.Count; i++)
+            {
+                Console.Write(lines[i] + "\t");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            //Question 10:
+            Console.WriteLine("Question 10");
+            string[] emails = { "dis.email+bull@usf.com", "dis.e.mail+bob.cathy@usf.com", "disemail+david@us.f.com" };
+            int unique_emails = NumUniqueEmails(emails);
+            Console.WriteLine("Number of Unique Mails: {0}", unique_emails);
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //Question 11:
+            Console.WriteLine("Question 11");
+            string bulls_string10 = "()[]{}";
+            bool isvalid = IsValid(bulls_string10);
+            if (isvalid)
+                Console.WriteLine("Valid String");
+            else
+                Console.WriteLine("String is not Valid");
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            //Question 12:
+            Console.WriteLine("Question 12");
+            string bulls_string12 = "/home/";
+            string spath = SimplifyPath(bulls_string12);
+            Console.WriteLine("Simplified path is {0}", spath);
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            //Question 13:
+            Console.WriteLine("Question 13");
+            string[] bulls_string13 = { "gin", "zen", "gig", "msg" };
+            int diffwords = UniqueMorseRepresentations(bulls_string13);
+            Console.WriteLine("Number of with unique code are: {0}", spath);
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //Question 14:
+            Console.WriteLine("Question 14");
+            List<List<int>> scores=new List<List<int>>();
+            List<List<int>> result = new List<List<int>>();
+            scores.Add(new List<int> { 1, 91 });scores.Add(new List<int> { 1, 92 });
+            scores.Add(new List<int> { 2, 93 });scores.Add(new List<int> { 2, 97 });
+            scores.Add(new List<int> { 1, 60 });scores.Add(new List<int> { 2, 77 });
+            scores.Add(new List<int> { 1, 65 });scores.Add(new List<int> { 1, 87 });
+            scores.Add(new List<int> { 1, 100 });scores.Add(new List<int> { 2, 100 });
+            scores.Add(new List<int> { 2, 76 });
+
+            result=HighFive(scores);
+
+            for (int i = 0; i < result.Count; i++)
+            {
+                Console.Write(result[i][0] +  "\t" + result[i][1]);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //Question 15:
+            Console.WriteLine("Question 11");
             int[,] grid = { { 0, 1, 2, 3, 4 }, { 24, 23, 22, 21, 5 }, { 12, 13, 14, 15, 16 }, { 11, 17, 18, 19, 20 }, { 10, 9, 8, 7, 6 } };
             int time = SwimInWater(grid);
             Console.WriteLine("Minimum time required is: {0}", time);
             Console.WriteLine();
 
-            //Question 10:
-            Console.WriteLine("Question 10");
+
+            //Question 16:
+            Console.WriteLine("Question 12");
             string word1  = "horse";
             string word2 = "ros";
             int minLen = MinDistance( word1,  word2);
@@ -361,9 +436,242 @@ namespace ISM6225_Assignment_2_Spring_2022
                 throw;
             }
         }
+
+        /*
+        Question 9
+
+        You are given a string s of lowercase English letters and an array widths denoting how many pixels wide each lowercase English letter is. Specifically, widths[0] is the width of 'a', widths[1] is the width of 'b', and so on.
+        You are trying to write s across several lines, where each line is no longer than 100 pixels. Starting at the beginning of s, write as many letters on the first line such that the total width does not exceed 100 pixels. Then, from where you stopped in s, continue writing as many letters as you can on the second line. Continue this process until you have written all of s.
+        Return an array result of length 2 where:
+             •	result[0] is the total number of lines.
+             •	result[1] is the width of the last line in pixels.
+ 
+        Example 1:
+        Input: widths = [10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10], s = "abcdefghijklmnopqrstuvwxyz"
+        Output: [3,60]
+        Explanation: You can write s as follows:
+                     abcdefghij  	 // 100 pixels wide
+                     klmnopqrst  	 // 100 pixels wide
+                     uvwxyz      	 // 60 pixels wide
+                     There are a total of 3 lines, and the last line is 60 pixels wide.
+
+
+
+         Example 2:
+         Input: widths = [4,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10], 
+         s = "bbbcccdddaaa"
+         Output: [2,4]
+         Explanation: You can write s as follows:
+                      bbbcccdddaa  	  // 98 pixels wide
+                      a           	 // 4 pixels wide
+                      There are a total of 2 lines, and the last line is 4 pixels wide.
+
+         */
+
+        public static List<int> NumberOfLines(int[] widths,string s)
+        {
+            try
+            {
+                //write your code here.
+
+                return new List<int>() { };
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+
+        /*
+         
+        Question 10
+
+        Every valid email consists of a local name and a domain name, separated by the '@' sign. Besides lowercase letters, the email may contain one or more '.' or '+'.
+            •	For example, in "rocky@usf.edu", "rocky" is the local name, and "usf.edu" is the domain name.
+        If you add periods '.' between some characters in the local name part of an email address, mail sent there will be forwarded to the same address without dots in the local name. Note that this rule does not apply to domain names.
+            •	For example, "bulls.z@usf.edu" and "bullsz@usf.edu" forward to the same email address.
+        If you add a plus '+' in the local name, everything after the first plus sign will be ignored. This allows certain emails to be filtered. Note that this rule does not apply to domain names.
+            •	For example, "m.y+name@email.com" will be forwarded to "my@email.com".
+        It is possible to use both of these rules at the same time.
+        Given an array of strings emails where we send one email to each emails[i], return the number of different addresses that actually receive mails.
+ 
+        Example 1:
+        Input: emails = ["dis.email+bull@usf.com","dis.e.mail+bob.cathy@usf.com","disemail+david@us.f.com"]
+        Output: 2
+        Explanation:"disemail@usf.com" and "disemail@us.f.com" actually receive mails
+
+        */
+
+        public static int NumUniqueEmails(string[] emails)
+        {
+            try
+            {
+                //write your code here.
+
+                return 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
         /*
         
-        Question 9:
+        Question 11
+
+        Given a string bulls_string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+        An input string is valid if:
+            1.	Open brackets must be closed by the same type of brackets.
+            2.	Open brackets must be closed in the correct order.
+ 
+        Example 1:
+        Input: bulls_string = "()"
+        Output: true
+
+        Example 2:
+        Input: bulls_string  = "()[]{}"
+        Output: true
+
+        Example 3:
+        Input: bulls_string  = "(]"
+        Output: false
+
+        */
+
+        public static bool IsValid(string bulls_string10)
+        {
+            try
+            {
+                //write your code here.
+
+                return false;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+
+        }
+
+        /*
+        Question 12
+        Given a string path, which is an absolute path (starting with a slash '/') to a file or directory in a Unix-style file system, convert it to the simplified canonical path.
+        In a Unix-style file system, a period '.' refers to the current directory, a double period '..' refers to the directory up a level, and any multiple consecutive slashes (i.e. '//') are treated as a single slash '/'. For this problem, any other format of periods such as '...' are treated as file/directory names.
+        The canonical path should have the following format:
+                •	The path starts with a single slash '/'.
+                •	Any two directories are separated by a single slash '/'.
+                •	The path does not end with a trailing '/'.
+                •	The path only contains the directories on the path from the root directory to the target file or directory (i.e., no period '.' or double period '..')
+        Return the simplified canonical path.
+ 
+        Example 1:
+        Input: path = "/home/"
+        Output: "/home"
+        Explanation: Note that there is no trailing slash after the last directory name.
+
+        Example 2:
+        Input: path = "/../"
+        Output: "/"
+        Explanation: Going one level up from the root directory is a no-op, as the root level is the highest level you can go.
+
+        */
+
+        public static string SimplifyPath(string path)
+        {
+            try
+            {
+                //write your code here.
+
+                return "";
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+
+        /*
+         Question 13
+        International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows:
+        •	'a' maps to ".-",
+        •	'b' maps to "-...",
+        •	'c' maps to "-.-.", and so on.
+
+        For convenience, the full table for the 26 letters of the English alphabet is given below:
+        [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        Given an array of strings words where each word can be written as a concatenation of the Morse code of each letter.
+            •	For example, "cab" can be written as "-.-..--...", which is the concatenation of "-.-.", ".-", and "-...". We will call such a concatenation the transformation of a word.
+        Return the number of different transformations among all words we have.
+ 
+        Example 1:
+        Input: words = ["gin","zen","gig","msg"]
+        Output: 2
+        Explanation: The transformation of each word is:
+        "gin" -> "--...-."
+        "zen" -> "--...-."
+        "gig" -> "--...--."
+        "msg" -> "--...--."
+        There are 2 different transformations: "--...-." and "--...--.".
+
+        */
+
+        public static int UniqueMorseRepresentations(string[] words)
+        {
+            try
+            {
+                //write your code here.
+
+                return 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+        /*Question 14
+         
+    Given a list of the scores of different USF students, items, where items[i] = [IDi, scorei] represents one score from a student with IDi, calculate each student's top five average.
+    Return the answer as an array of pairs result, where result[j] = [IDj, topFiveAveragej] represents the student with IDj and their top five average. Sort result by IDj in increasing order.
+    A student's top five average is calculated by taking the sum of their top five scores and dividing it by 5 using integer division.
+ 
+    Example 1:
+    Input: items = [[1,91],[1,92],[2,93],[2,97],[1,60],[2,77],[1,65],[1,87],[1,100],[2,100],[2,76]]
+    Output: [[1,87],[2,88]]
+    Explanation: 
+    The student with ID = 1 got scores 91, 92, 60, 65, 87, and 100. Their top five average is (100 + 92 + 91 + 87 + 65) / 5 = 87.
+    The student with ID = 2 got scores 93, 97, 77, 100, and 76. Their top five average is (100 + 97 + 93 + 77 + 76) / 5 = 88.6, but with integer division their average converts to 88.
+
+        */
+
+        public static List<List<int>> HighFive(List<List<int>> items)
+        {
+            try
+            {
+                List<List<int>> highFive = new List<List<int>>();
+                //write your code here.
+
+                return highFive;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+
+        /*
+        
+        Question 15:
         You are given an n x n integer matrix grid where each value grid[i][j] represents the elevation at that point (i, j).
         The rain starts to fall. At time t, the depth of the water everywhere is t. You can swim from a square to another 4-directionally adjacent square if and only if the elevation of both squares individually are at most t. You can swim infinite distances in zero time. Of course, you must stay within the boundaries of the grid during your swim.
         Return the least time until you can reach the bottom right square (n - 1, n - 1) if you start at the top left square (0, 0).
@@ -392,7 +700,7 @@ namespace ISM6225_Assignment_2_Spring_2022
 
         /*
          
-        Question 10:
+        Question 16:
         Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
         You have the following three operations permitted on a word:
 
