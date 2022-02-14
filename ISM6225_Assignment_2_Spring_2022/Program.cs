ad2b0e1e25c47b0aee7bf6f1d1fd549b4c1f0f74
yagnakamatham <36106651+yagnakamatham@users.noreply.github.com>
2022-02-13 20:16:37 -0800

    Add files via upload

--- ISM6225_Assignment_2_Spring_2022/Program.cs
+++ ISM6225_Assignment_2_Spring_2022/Program.cs
@@ -15,7 +15,6 @@ namespace ISM6225_Assignment_2_Spring_2022
     {
         static void Main(string[] args)
         {
-
             //Question 1:
             Console.WriteLine("Question 1:");
             int[] nums1 = { 0, 1, 2, 3, 12 };
@@ -103,13 +102,13 @@ namespace ISM6225_Assignment_2_Spring_2022
 
             //Question 10:
             Console.WriteLine("Question 10");
-            string word1  = "horse";
+            string word1 = "horse";
             string word2 = "ros";
-            int minLen = MinDistance( word1,  word2);
+            int minLen = MinDistance(word1, word2);
             Console.WriteLine("Minimum number of operations required are {0}", minLen);
             Console.WriteLine();
         }
-    
+
 
         /*
         
@@ -132,8 +131,33 @@ namespace ISM6225_Assignment_2_Spring_2022
         {
             try
             {
-                //Write your Code here.
-                return -1;
+
+                int size = nums.Length;
+
+                // limits for searching : low and high
+                int low = 0;
+                int high = size - 1;
+
+                int mid = low + (high - low) / 2;         // middle of low and high
+
+                while (low <= high)
+                {       // run until limits overlap
+
+                    if (nums[mid] < target)          // if less than target, cut the search space to left half 
+                        low = mid + 1;
+
+                    else if (nums[mid] > target)         // if greater than target, cut the space to right half
+                        high = mid - 1;
+
+                    if (nums[mid] == target)         // if equal, then return the index
+                        return mid;
+
+                    mid = low + (high - low) / 2;            // calculate the middle 
+                }
+
+                if (target > nums[mid])          // check's the position to be placed given that we didnot find the target: low/ high
+                    return low;
+                return high;
             }
             catch (Exception)
             {
@@ -163,14 +187,46 @@ namespace ISM6225_Assignment_2_Spring_2022
         {
             try
             {
-                
+
                 //write your code here.
 
-                return "";
+                // remove punctuations and make lowercase      
+                paragraph = paragraph.Replace(".", "");
+                paragraph = paragraph.Replace(",", "");
+                paragraph = paragraph.ToLower();
+
+                string[] arr = paragraph.Split();         // split the paragraph into words: delimited by " "
+
+                // defining a dictionary for words and their occurance count
+                Dictionary<string, int> d = new Dictionary<string, int>();
+
+                for (int i = 0; i < arr.Length; i++)
+                {      // iterate through the string array
+                    if (Array.IndexOf(banned, arr[i]) == -1)
+                    {      // check if it is a banned word or not
+                        if (d.ContainsKey(arr[i]) == true)     // if key already present, then increment the counter or else just add it to dict
+                            d[arr[i]] += 1;
+                        else
+                            d.Add(arr[i], 1);
+                    }
+                }
+
+                int maxval = -1;
+
+                string ret = "";
+
+                foreach (KeyValuePair<string, int> kvp in d)
+                {   // search through kvps in the dict
+                    if (kvp.Value >= maxval)
+                    {                       // if the value we have is greater, then replace it
+                        ret = kvp.Key;
+                        maxval = kvp.Value;
+                    }
+                }
+                return ret;     // return the string
             }
             catch (Exception)
             {
-
                 throw;
             }
         }
@@ -200,8 +256,29 @@ namespace ISM6225_Assignment_2_Spring_2022
         {
             try
             {
-                //write your code here.
-                return 0;
+                //write your code here.\
+
+                // defining a dictionary
+                Dictionary<int, int> d = new Dictionary<int, int>();
+
+                for (int i = 0; i < arr.Length; i++)
+                {   // update the dictionary with numbers and their counts
+                    if (!d.ContainsKey(arr[i]))
+                        d.Add(arr[i], 1);
+                    else
+                        d[arr[i]] += 1;
+                }
+
+                int lucky = -1;      // initialize a var for result
+                foreach (KeyValuePair<int, int> kvp in d)
+                {
+                    if (kvp.Value == kvp.Key)
+                    {         // if the frequency and the number are equal, then it's lucky
+                        if (lucky < kvp.Key)             // if we already have a lucky no, check which one is greater
+                            lucky = kvp.Key;
+                    }
+                }
+                return lucky;       // return the number
             }
             catch (Exception)
             {
@@ -236,7 +313,45 @@ namespace ISM6225_Assignment_2_Spring_2022
             try
             {
                 //write your code here.
-                return "";
+                int size = secret.Length;         // get the size of the string
+
+                // defining a dictionary
+                Dictionary<char, int> d = new Dictionary<char, int>();
+
+                for (int i = 0; i < size; i++)
+                {       // map the elements to their counts
+                    if (!d.ContainsKey(secret[i]))
+                        d.Add(secret[i], 1);
+                    else
+                        d[secret[i]] += 1;
+                }
+
+                int bulls_counter, cows_counter;
+
+                bulls_counter = cows_counter = 0;
+
+                for (int i = 0; i < size; i++)
+                {           // count the bulls in the string: by checking the char same or not
+                    if (secret[i] == guess[i])
+                    {
+                        bulls_counter++;
+                        d[guess[i]] = d[guess[i]] - 1;          // decrement the val in dict so that we don't add it to cow counter
+                    }
+                }
+
+                for (int i = 0; i < size; i++)
+                {             // count the number of cows in the string by checking in the dictionary 
+                    if (secret[i] != guess[i])
+                    {
+                        if (d.ContainsKey(guess[i]) == true)
+                        {
+                            if (d[guess[i]] > 0)
+                                cows_counter++;
+                        }
+                    }
+                }
+                return bulls_counter.ToString() + "A" + cows_counter.ToString() + "B";        // return the result in the required format
+
             }
             catch (Exception)
             {
@@ -266,8 +381,46 @@ namespace ISM6225_Assignment_2_Spring_2022
             try
             {
                 //write your code here.
-                
-                return new List<int>() {} ;
+                List<int> li = new List<int>();
+
+                int pindex = 0;        // variable that stores the index where the partition ends
+                int size = s.Length;
+
+                // defining a dictionary
+                Dictionary<char, int> d = new Dictionary<char, int>();
+
+                int prev = 0;
+
+
+                for (int i = 0; i < size;)
+                {
+
+                    char cur = s[i];  // first char of this partition
+
+                    for (int j = i; j < size; j++)
+                    {
+                        if (d.ContainsKey(s[j]) == true)
+                        {
+                            if (d[s[j]] <= pindex || cur == s[j])   // if the char already occured, or if it's the cur again,
+                                pindex = j;        // change the partitioning index limit
+                            d[s[j]] = j;
+                        }
+                        else
+                        {
+                            d.Add(s[j], j);         // add to the dict
+                        }
+                    }
+                    int ind = pindex + 1;             // this will be added to the list li
+                    if (i != 0)
+                    {
+                        ind = ind - prev;           // get the size
+                    }
+                    li.Add(ind);                // add to the list li
+                    i = pindex + 1;                 // update the i
+                    prev = pindex + 1;
+                }
+                return li;
+
             }
             catch (Exception)
             {
@@ -306,13 +459,35 @@ namespace ISM6225_Assignment_2_Spring_2022
 
          */
 
-        public static List<int> NumberOfLines(int[] widths,string s)
+        public static List<int> NumberOfLines(int[] widths, string s)
         {
             try
             {
                 //write your code here.
 
-                return new List<int>() { };
+                // defining a dictionary
+                Dictionary<char, int> d = new Dictionary<char, int>();
+
+                for (int i = 0; i < widths.Length; i++)
+                {      // fill the dictionary with the letter s mapped to their corresponding widths
+                    d.Add((char)((int)'a' + i), widths[i]);
+                }
+
+                int c, l;
+                c = l = 0;
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (i == 0) l++;
+                    if (c + d[s[i]] > 100)
+                    {       // if the count is exceeding 100 pixels,
+                        c = d[s[i]];             // increment the line
+                        l++;
+                    }
+                    else
+                        c = c + d[s[i]];	        // else, just add to c
+                }
+
+                return new List<int>() { l, c };      // return output
             }
             catch (Exception)
             {
@@ -349,8 +524,32 @@ namespace ISM6225_Assignment_2_Spring_2022
         {
             try
             {
-                //write your code here.
 
+                int size = bulls_string10.Length;
+                // define var s for all types of brackets
+                int nr = 0;
+                int sq = 0;
+                int cr = 0;
+
+                // if we come across an open bracket, then increment the counter, or else just decrement. IF at end, the value is 0, then it's balanced
+
+                for (int i = 0; i < size; i++)
+                {
+                    if (bulls_string10[i] == '(')
+                        nr++;
+                    else if (bulls_string10[i] == ')')
+                        nr--;
+                    else if (bulls_string10[i] == '[')
+                        sq++;
+                    else if (bulls_string10[i] == ']')
+                        sq--;
+                    else if (bulls_string10[i] == '{')
+                        cr++;
+                    else if (bulls_string10[i] == '}')
+                        cr--;
+                }
+                if (nr == 0 && sq == 0 && cr == 0)       // as explained above, if all 0, then true else false   
+                    return true;
                 return false;
             }
             catch (Exception)
@@ -392,9 +591,40 @@ namespace ISM6225_Assignment_2_Spring_2022
         {
             try
             {
-                //write your code here.
 
-                return 0;
+                // write the morse codes for all letters
+                string[] mcodes = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
+
+
+                // define a dictionary for letters mapping to morse
+                Dictionary<char, string> map = new Dictionary<char, string>();
+
+                for (int i = 0; i < mcodes.Length; i++)     // fill the above dictionary
+                    map.Add((char)((int)'a' + i), mcodes[i]);
+
+
+                // define a dictionary for unique morse coeds
+
+                Dictionary<string, int> mdist = new Dictionary<string, int>();
+
+
+                for (int i = 0; i < words.Length; i++)
+                {
+
+                    string word = words[i].ToLower();
+                    string mc = "";
+
+                    for (int j = 0; j < word.Length; j++)    // add the individual morse codes of all the chars so we get the final code for the string
+                        mc += map[word[j]];
+                    // add to dict so finally we can just get the keys and they will say how many distinct codes there are
+                    if (mdist.ContainsKey(mc) == true)
+                        mdist[mc] += 1;
+                    else
+                        mdist.Add(mc, 1);
+
+                }
+
+                return mdist.Count;  // return the number of keys in the dictionary.
             }
             catch (Exception)
             {
@@ -403,7 +633,7 @@ namespace ISM6225_Assignment_2_Spring_2022
 
         }
 
-      
+
 
 
         /*
@@ -425,12 +655,10 @@ namespace ISM6225_Assignment_2_Spring_2022
         {
             try
             {
-                //write your code here.
                 return 0;
             }
             catch (Exception)
             {
-
                 throw;
             }
         }
@@ -460,9 +688,30 @@ namespace ISM6225_Assignment_2_Spring_2022
         {
             try
             {
-                //write your code here.
-                return 0;
+                int len1 = word1.Length;          // length of two strings in two vars
+                int len2 = word2.Length;
 
+                int[,] DP = new int[len1 + 1, len2 + 1];     // 2darray for bottom up dynamic programming approach
+
+                for (int i = 0; i <= len1; i++)
+                {               // bottom up approach for dp : start from bottom and fill to the top using previously stored subproblems/ entries
+
+                    for (int j = 0; j <= len2; j++)
+                    {
+
+                        if (i == 0 && j == 0)            // string len is 0, so just dp[i,j] = 0
+                            DP[i, j] = 0;
+                        else if (i == 0)                 // if word1 s len is 0, then we req j (word2 len) ops
+                            DP[i, j] = j;
+                        else if (j == 0)                 // if word2 s len is 0, then we req i (word1 len) ops
+                            DP[i, j] = i;
+                        else if (word1[i - 1] == word2[j - 1])  // if the last char of both the strings are same, then just dp[i-1,j-1]
+                            DP[i, j] = DP[i - 1, j - 1];
+                        else                                // else, then calc the minimum of all the three ops (intsert, remove, replace) on the word1
+                            DP[i, j] = 1 + Math.Min(DP[i - 1, j - 1], Math.Min(DP[i, j - 1], DP[i - 1, j]));
+                    }
+                }
+                return DP[len1, len2];       // return the dp[len1,len2] meaning the min opereations req for word1 to change to word2;
             }
             catch (Exception)
             {
